52b56c0568a7490af93cbab1f7381741e4948ce6
Kompotus <mihavitrik@gmail.com>
2024-08-28 11:24:18 +0300

    Edited project name

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { ConfigProvider } from './context/ConfigContext.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ConfigProvider>
-      <BrowserRouter basename='/tg-app-front/'>
+      <BrowserRouter basename='/RoxyTap/'>
         <App />
       </BrowserRouter>
     </ConfigProvider>
